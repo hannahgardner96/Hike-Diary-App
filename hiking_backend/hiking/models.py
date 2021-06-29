5815bee1e8f803a269f29d5485da1246de99fe3d
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-06-29 15:10:28 -0500

    models for hike location, hike recommendation, and hike diary complete

--- hiking_backend/hiking/models.py
+++ hiking_backend/hiking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class HikeLocation(models.Model):
+    hike_name = models.CharField(max_length = 100)
+    hike_address = models.CharField(max_length = 100)
+    hike_lat = models.DecimalField(max_digits=20, decimal_places=20)
+    hike_lng = models.DecimalField(max_digits=20, decimal_places=20)
+
+class HikeRecommendation(models.Model):
+    hike_location = models.ForeignKey(HikeLocation, on_delete = models.CASCADE)
+    hike_img = models.CharField(max_length = 200)
+    weather_rating = models.IntegerField()
+
+class DiaryEntry(models.Model):
+    hike_location = models.ForeignKey(HikeLocation, on_delete = models.PROTECT)
+    hike_date = models.DateField(default = date.today)
+    hike_description = models.TextField()
+    hike_weather_rating = models.IntegerField()
+
+
+# referenced https://stackoverflow.com/questions/2029295/django-datefield-default-options for setting default date to today and improting datetime 
+# referenced https://www.geeksforgeeks.org/django-model-data-types-and-fields-list/ for required arguments in fields
--- hiking_backend/hiking_backend/settings.py
+++ hiking_backend/hiking_backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hiking.apps.HikingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
