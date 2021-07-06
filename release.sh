6af9511c2701ef1144fa15f98d88df90c5de337a
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-07-06 10:46:48 -0500

    bundle on local machine instead of dealing with heroku buildpacks

--- release.sh
+++ release.sh
@@ -1,10 +1,3 @@
-# React stuff
-cd hiking_frontend
-npm i 
-npm run build
-cp -a build/. ../hiking_backend/hiking_backend/static/
-# above line copies the files produced by the npm run build (static files) and copies them into the following folder (where we told Django to look for static files)
-
 # Python stuff
 cd ..
 pip install -r requirements.txt 
