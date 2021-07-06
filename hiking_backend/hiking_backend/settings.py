88d887a346775d28bd8a93714586966e5daf8593
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-07-06 10:31:26 -0500

    addressing issue with conflicting MEDIA_URL and STATIC_URL

--- hiking_backend/hiking_backend/settings.py
+++ hiking_backend/hiking_backend/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.abspath('./static/')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
