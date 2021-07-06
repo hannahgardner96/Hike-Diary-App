fe2e7c6fbfac8ebf4fdbfb42e702423afc2c89d7
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-07-06 10:48:36 -0500

    adding untracked files

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+# React stuff
+cd hiking_frontend
+npm i 
+npm run build
+cp -a build/. ../hiking_backend/hiking_backend/static/
+# above line copies the files produced by the npm run build (static files) and copies them into the following folder (where we told Django to look for static files)
+
+git commit -am "Bundling app for Heroku deployment"
+git push heroku
