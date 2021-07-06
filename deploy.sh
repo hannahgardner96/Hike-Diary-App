2c079456da20cb6192838cc017ddf0c014797d53
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-07-06 10:51:43 -0500

    no need to npm i in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 # React stuff
 cd hiking_frontend
-npm i 
 npm run build
 cp -a build/. ../hiking_backend/hiking_backend/static/
 # above line copies the files produced by the npm run build (static files) and copies them into the following folder (where we told Django to look for static files)
