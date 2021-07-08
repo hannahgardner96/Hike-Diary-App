9e699a3dc57a98313884888e1e5b21a9b6663ba8
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-07-07 19:35:02 -0500

    Bundling app for Heroku deployment

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 # Python stuff
 cd hiking_backend
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 # now, the static folder contains both information from react static files and postgres db files 
